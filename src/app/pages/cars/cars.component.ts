8de816915c7cffdae2d6221bc383fe9c05121fc6
missprempree <delenies@gmail.com>
2024-12-25 11:47:19 +0700

    Revise Save, Update, Delete, Reload

--- src/app/pages/cars/cars.component.ts
+++ src/app/pages/cars/cars.component.ts
@@ -13,6 +13,18 @@ import { CommonModule } from '@angular/common';
 
 export class CarsComponent {
 
+  // Initial state
+  initialCarFormData = {
+    carId: 0,
+    brand: "",
+    model: "",
+    year: 0,
+    color: "",
+    dailyRate: 0,
+    carImage: "",
+    regNo: ""
+  };
+
   carFormData = signal({
       carId: 0,
       brand: "",
@@ -30,10 +42,29 @@ export class CarsComponent {
   carList$: Observable<any[]> = new Observable<any[]>;
   
   apiUrl: string = "api/api/CarRentalApp/";
+  // apiUrl2: string = "/api/CarRentalApp/";
 
   constructor(){
+    this.loadCarList()
+    /*
     this.carList$ = this.http.get<any[]>(`${this.apiUrl}GetCars`).pipe(
       map((result:any)=>{
+        if (!result || !result.data) {
+          throw new Error('Unexpected response structure')
+        }
+        return result.data
+      }),
+      catchError((error) => {
+        console.error('API error:', error)
+        return of([]) // Return empty array on error
+      })
+    );*/
+  }
+
+   // Method to update the carList$ observable
+   loadCarList() {
+    this.carList$ = this.http.get<any[]>(`${this.apiUrl}GetCars`).pipe(
+      map((result: any) => {
         if (!result || !result.data) {
           throw new Error('Unexpected response structure');
         }
@@ -45,6 +76,7 @@ export class CarsComponent {
       })
     );
   }
+
   /*
   getUser(id: string): Observable<User> {
     return this.http.get<User>(`/api/user/${id}`);
@@ -57,29 +89,39 @@ export class CarsComponent {
     this.http.post(`${this.apiUrl}CreateNewCar`,this.carFormData()).subscribe((resultData:any)=>{
       if(resultData.result){
         alert("Car has been created successful!")
+        this.resetForm()
+        this.loadCarList() // Reload the car list
       } else {
         alert(resultData.message)
       }
     })
   }
 
-    
+  // Reset Form
   resetForm() {
-    /*
-    this.carFormData = {
-      regNo: "",
-      brand: "",
-      model: "",
-      year: 0,
-      color: "",
-      dailyRate: 0,
-    };*/
+      this.carFormData.set({ ...this.initialCarFormData })
   }
 
   onEdit(data: any) {
-    this.carFormData.set(data);  // Update the signal with the new data
+      this.carFormData.set(data)  // Update the signal with the new data
   }
 
+  onAdd() {
+    this.resetForm()
+    this.carFormData()
+  }
+
+  onUpdate() {
+    this.http.put(`${this.apiUrl}UpdateCar`,this.carFormData()).subscribe((resultObj:any)=>{
+      if (resultObj.result) {
+          alert("Car has been updated successfully!")
+          this.loadCarList() // Reload the car list
+          //this.resetForm()
+      }else{
+          alert(resultObj.message)
+      }
+    })
+  }
   
   onDelete(id: number) {
     const isDelete = confirm("Are you sure wanting to Delete?");
@@ -88,6 +130,7 @@ export class CarsComponent {
         if (resultObj.result) {
             alert("Car has been deleted successfully!")
             this.resetForm()
+            this.loadCarList() // Reload the car list
         } else {
             alert(resultObj.message)
         }
